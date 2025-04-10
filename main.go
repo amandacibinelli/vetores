e1b68c2de59e40e37a229bcbbe86ead50b9764ac
amanda <amanda.cibinelli@aluno.senai.br>
2025-04-10 09:38:10 -0300

    estrutura de decisao

--- main.go
+++ main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
-	"fmt"
+"fmt"
+
 )
 
-func main(){
-    
-    var numeros [5]int
-    var soma int
+ func main(){
+ages:=45
+fmt.Println(ages<=50)
+fmt.Println(ages >=50)
+fmt.Println(ages ==50)
+fmt.Println(ages !=50)
 
-    fmt.Println("Digite 5 numeros inteiros:")
-    for i := 0; i < 5; i++ {
-        fmt.Println("Numero %d: ", i+1)
-       fmt.Scan(&numeros[i])
-        soma += numeros[i]
-    }
-
-    fmt.Println("A soma dos números é: %d\n", soma)
-
+if ages <30{
+    fmt.Println("menor que 30 anos")
+}else if ages <40{
+ fmt.Println("menor que 40 anos ")
+}else {
+    fmt.Println("não é menor que 40 anos")
 }
+names := []string{"Isadora", "Yasmim","Trunks", "Cebolinha", " Martin","Yuri"}
+
+for index, value := range names {
+    if index== 1{
+        fmt.Println(" Continue apos a posição ", index, " e o valor ", value)
+        continue 
+    }
+    if index >2 {
+        fmt.Println("sair após", index)
+        break
+    }
+    fmt.Println("Valor:",value)
+}
+ }
