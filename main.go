869e1c8cfcef591e12f8227835079beef3a4bd98
amanda <amanda.cibinelli@aluno.senai.br>
2025-04-17 08:32:50 -0300

    soma, mult, sub

--- main.go
+++ main.go
@@ -5,19 +5,33 @@ import (
 
 )
 
-func dividir(dividendo int,divisor int) (int, string) { 
 
-if divisor ==0{
-return 0, "erro na divisão por zero"
-} 
-return dividendo/divisor, "Sem erro"
+func dividir(dividendo int, divisor int)(int, string){
+    if divisor == 0 {
+        return 0, "Erro na divisão por zero"
+    }
+    return dividendo / divisor, "Sem erro"
 
-}
- func main() {
- resultado, erro:= dividir(10,2)
- if erro != "sem erro" {
-    fmt.Println(erro)
- } else {
-    fmt.Println("O resultado da divisão eh:" , resultado)
- }
- }
+  }
+  func operacaoBasica(a int, b int ) (int , int ,int ){
+    soma := a+b
+    multiplicacao := a * b
+    subtracao := a - b
+    return soma , multiplicacao , subtracao
+
+  }
+
+  func main(){
+    resultado, erro := dividir(10,2)
+
+    if erro != "Sem erro"{
+        fmt.Println(erro)
+    }else {
+        fmt.Println("O resultado da  divisão é :", resultado)
+    }
+    soma , mult , sub := operacaoBasica(10,2)
+    fmt.Println(soma)
+    fmt.Println(mult)
+    fmt.Println(sub)
+
+  }
