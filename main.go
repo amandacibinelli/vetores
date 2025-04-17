f833e36776c2a7e1cfc2c1831b3f1552faf23afa
amanda <amanda.cibinelli@aluno.senai.br>
2025-04-17 08:36:06 -0300

    maior ou menor de idade

--- main.go
+++ main.go
@@ -4,34 +4,14 @@ import (
 	"fmt"
 
 )
+func dadosPessoa(nome string, idade int) (int, string) {
+	if idade >= 18 {
+		return idade, "voce eh maior de idade"
+	}
+	return idade, "voce eh menor de idade"
+}
 
-
-func dividir(dividendo int, divisor int)(int, string){
-    if divisor == 0 {
-        return 0, "Erro na divisão por zero"
-    }
-    return dividendo / divisor, "Sem erro"
-
-  }
-  func operacaoBasica(a int, b int ) (int , int ,int ){
-    soma := a+b
-    multiplicacao := a * b
-    subtracao := a - b
-    return soma , multiplicacao , subtracao
-
-  }
-
-  func main(){
-    resultado, erro := dividir(10,2)
-
-    if erro != "Sem erro"{
-        fmt.Println(erro)
-    }else {
-        fmt.Println("O resultado da  divisão é :", resultado)
-    }
-    soma , mult , sub := operacaoBasica(10,2)
-    fmt.Println(soma)
-    fmt.Println(mult)
-    fmt.Println(sub)
-
-  }
+func main() {
+	idade, status := dadosPessoa("Amanda", 16)
+	fmt.Printf("Idade: %d, Status: %s\n", idade, status)
+}
