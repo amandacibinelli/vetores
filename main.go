6cff79080c2b16585284acb3e6af8fec5fd3e55d
amanda <amanda.cibinelli@aluno.senai.br>
2025-04-17 08:45:36 -0300

    maior ou menor de idade2

--- main.go
+++ main.go
@@ -5,13 +5,28 @@ import (
 
 )
 func dadosPessoa(nome string, idade int) (int, string) {
+	var status string
 	if idade >= 18 {
-		return idade, "voce eh maior de idade"
+		status = "Maior de idade"
+	} else {
+		status = "Menor de idade"
 	}
-	return idade, "voce eh menor de idade"
+	return idade, status
 }
 
 func main() {
-	idade, status := dadosPessoa("Amanda", 16)
-	fmt.Printf("Idade: %d, Status: %s\n", idade, status)
+	var nome string
+	var idade int
+
+	fmt.Print("Digite o nome: ")
+	fmt.Scanln(&nome)
+
+	fmt.Print("Digite a idade: ")
+	fmt.Scanln(&idade)
+
+	idadeRetornada, status := dadosPessoa(nome, idade)
+
+	fmt.Printf("Nome: %s\n", nome)
+	fmt.Printf("Idade: %d\n", idadeRetornada)
+	fmt.Printf("Status: %s\n", status)
 }
