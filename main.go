776e11b24ea43d1df3c42c7665341272367b718b
amanda <amanda.cibinelli@aluno.senai.br>
2025-04-17 08:20:40 -0300

    Erro na divisao

--- main.go
+++ main.go
@@ -5,100 +5,19 @@ import (
 
 )
 
-func main() { 
+func dividir(dividendo int,divisor int) (int, string) { 
 
-    var saldo float32 
-
-    fmt.Println("Insira o valor inicial:") 
-
-    fmt.Scan(&saldo) 
-
- 
-
-    escolherOperacao(&saldo) 
-
+if divisor ==0{
+return 0, "erro na divisão por zero"
 } 
+return dividendo/divisor, "Sem erro"
 
- 
-
-func escolherOperacao(saldo *float32) { 
-
-    var opcao int 
-
-    fmt.Println("Você deseja sacar (1), depositar (2) ou encerrar a sessão (3)?") 
-
-    fmt.Scan(&opcao) 
-
- 
-
-    switch opcao { 
-
-    case 1: 
-
-        sacar(saldo) 
-
-    case 2: 
-
-        depositar(saldo) 
-
-    case 3: 
-
-        encerrar() 
-
-    default: 
-
-        fmt.Println("Opção inválida.") 
-
-    } 
-
-} 
-
- 
-
-func depositar(saldo *float32) { 
-
-    var valor float32 
-
-    fmt.Println("Qual valor deseja depositar?") 
-
-    fmt.Scan(&valor) 
-
-    *saldo += valor 
-
-    fmt.Println("Seu saldo atual é:", *saldo) 
-
-} 
-
- 
-
-func sacar(saldo *float32) { 
-
-    var valor float32 
-
-    fmt.Println("Qual valor deseja sacar?") 
-
-    fmt.Scan(&valor) 
-
-    if valor > *saldo { 
-
-        fmt.Println("Saldo insuficiente.") 
-
-    } else { 
-
-        *saldo -= valor 
-
-        fmt.Println("Seu saldo atual é:", *saldo) 
-
-    } 
-
-} 
-
- 
-
-func encerrar() { 
-
-    fmt.Println("Sessão encerrada.") 
-
-} 
-
- 
+}
+ func main() {
+ resultado, erro:= dividir(10,2)
+ if erro != "sem erro" {
+    fmt.Println(erro)
+ } else {
+    fmt.Println("O resultado da divisão eh:" , resultado)
+ }
+ }
