db870f6024a2083653a96dc6fb2db36c15f50941
amanda <amanda.cibinelli@aluno.senai.br>
2025-04-24 13:26:38 -0300

    nome e sobrenome

--- main.go
+++ main.go
@@ -4,23 +4,14 @@ import (
 	"fmt"
 
 )
+func pegarNome() (string, string) {
+    return "Barry" , "Allen"
+}
 
-func main() {
-	alunoIdade := make(map[string]int)
-    alunoIdade["Bruno"] = 15
-    alunoIdade["Otavio"]= 16
-    alunoIdade["Fabiano"] = 40
-    alunoIdade["Isabela"] = 15
-    fmt.Println("Idade do Bruno", alunoIdade["Bruno"])
-    notasAlunos :=map[string]float64{
-        "Bruno" : 9.7,
-        "Otavio" :10,
-        "Fabiano":8.7,
-        "Isabela": 9.5,
 
-    }
-    for k, v:= range notasAlunos {
-        fmt.Printf("%s tirou a nota % 1.f ", k ,v )
-    }
-  }
+func main(){
+    nome, sobrenome := pegarNome()
+        fmt.Printf(nome)
+        fmt.Printf(sobrenome)
 
+}
